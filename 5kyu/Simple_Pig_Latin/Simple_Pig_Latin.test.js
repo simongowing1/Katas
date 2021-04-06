36ce8b00f3f8a40b00a73b308d196d41962def36
Simon Gowing <simongowing1@gmail.com>
2021-04-06 15:50:34 +0200

    feat(tests): implement tests for 5kyu katas

---
+++ 5kyu/Simple_Pig_Latin/Simple_Pig_Latin.test.js
@@ -0,0 +1,19 @@
+import {pigIt} from './Simple_Pig_Latin'
+
+describe('Simple Pig Latin', () => {
+    describe('pigIt', () => {
+        it('correctly adds ay to the end of a word', () => {
+            const text = 'foo'
+            expect(pigIt(text)).toEqual('oofay')
+        })
+
+        describe('when the text ends with a punctuation point', () => {
+            it('does not affect the punctuation point', () => {
+                const text = 'O tempora o mores !'
+                const result = 'Oay emporatay oay oresmay !'
+
+                expect(pigIt(text)).toEqual(result)
+            })
+        })
+    })
+})
---
+++ 5kyu/Valid_Parentheses/Valid_Parentheses.test.js
@@ -0,0 +1,21 @@
+import {validParentheses} from './Valid_Parentheses'
+
+describe('Valid Parentheses', () => {
+    describe('validParentheses', () => {
+        it('Takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it is invalid.', () => {
+            const string = '())'
+            expect(validParentheses(string)).toEqual(false)
+        })
+
+        it('Takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it is invalid.', () => {
+            const string = '())(()'
+            expect(validParentheses(string)).toEqual(false)
+        })
+
+        it('Takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it is invalid.', () => {
+            const string = '()'
+            expect(validParentheses(string)).toEqual(true)
+        })
+
+    })
+})
