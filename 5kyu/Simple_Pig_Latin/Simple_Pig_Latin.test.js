bbeab3cad0b5eaa51cb591daac49d807f0697796
Simon Gowing <simongowing1@gmail.com>
2021-05-24 12:33:18 +0200

    refactor: correct test syntax in 5kyu kata folders

--- 5kyu/Simple_Pig_Latin/Simple_Pig_Latin.test.js
+++ 5kyu/Simple_Pig_Latin/Simple_Pig_Latin.test.js
@@ -1,19 +1,20 @@
-import {pigIt} from './Simple_Pig_Latin'
+import { expect, describe, it } from '@jest/globals';
+import { pigIt } from './Simple_Pig_Latin';
 
 describe('Simple Pig Latin', () => {
-    describe('pigIt', () => {
-        it('correctly adds ay to the end of a word', () => {
-            const text = 'foo'
-            expect(pigIt(text)).toEqual('oofay')
-        })
+  describe('pigIt', () => {
+    it('correctly adds ay to the end of a word', () => {
+      const text = 'foo';
+      expect(pigIt(text)).toEqual('oofay');
+    });
 
-        describe('when the text ends with a punctuation point', () => {
-            it('does not affect the punctuation point', () => {
-                const text = 'O tempora o mores !'
-                const result = 'Oay emporatay oay oresmay !'
+    describe('when the text ends with a punctuation point', () => {
+      it('does not affect the punctuation point', () => {
+        const text = 'O tempora o mores !';
+        const result = 'Oay emporatay oay oresmay !';
 
-                expect(pigIt(text)).toEqual(result)
-            })
-        })
-    })
-})
+        expect(pigIt(text)).toEqual(result);
+      });
+    });
+  });
+});
