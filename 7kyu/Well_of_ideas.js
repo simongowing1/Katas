ce271ea267dfa7364708d8c6abd26cfa87e35bee
Simon Gowing <simongowing1@gmail.com>
2021-06-10 12:29:53 +0200

    refactor(7kyu): add streamlined solution to Well_of_ideas that does not use loops

--- 7kyu/Well_of_ideas.js
+++ 7kyu/Well_of_ideas.js
@@ -1,22 +1,36 @@
 // https://www.codewars.com/kata/57f22b0f1b5432ff09001cab/solutions/javascript
 
-// REFACTORED SOLUTION
+// 'WITHOUT LOOPS' SOLUTION
 
 function well(x) {
-  // declare an empty array
-  const justGoods = [];
-  // iterate through the argument to create one array of all the reviews called 'allReviews'
-  x.forEach((subArray) =>
-  // filter each subarray and push only the values that include 'good' into the new array
-    justGoods.push(...subArray.filter((value) =>
-    // make values into strings
-      value.toString()
-      // make lowercase inorder to compare to 'good'
-        .toLowerCase() === 'good')));
-  if (justGoods.length === 0) return 'Fail!';
-  if (justGoods.length <= 2) return 'Publish!';
-  if (justGoods.length > 2) return 'I smell a series!';
+  // Declare a variable of the argument array flattened to a string
+  const numberOfGoods = x.toString()
+  // Split it using the word 'good' (case insensitive) as the seperator
+    .split(/good/gi)
+    // find out the number of goods by finding the length of the resultant array and -1
+    .length - 1;
+  if (numberOfGoods > 2) return 'I smell a series!';
+  if (numberOfGoods > 0) return 'Publish!';
+  return 'Fail!';
 }
+
+// // REFACTORED SOLUTION
+
+// function well(x) {
+//   // declare an empty array
+//   const justGoods = [];
+//   // iterate through the argument to create one array of all the reviews called 'allReviews'
+//   x.forEach((subArray) =>
+//   // filter each subarray and push only the values that include 'good' into the new array
+//     justGoods.push(...subArray.filter((value) =>
+//     // make values into strings
+//       value.toString()
+//       // make lowercase inorder to compare to 'good'
+//         .toLowerCase() === 'good')));
+//   if (justGoods.length === 0) return 'Fail!';
+//   if (justGoods.length <= 2) return 'Publish!';
+//   if (justGoods.length > 2) return 'I smell a series!';
+// }
 
 // ORIGINAL SOLUTION
 // function well(x) {
