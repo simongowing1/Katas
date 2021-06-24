d600a180138f49045104205414e26e9de3d71a5b
Simon Gowing <simongowing1@gmail.com>
2021-06-24 16:52:42 +0200

    refactor(javascript_developers_coming_from_europe): streamline solution to use filter

--- 7kyu/JavaScript_developers_coming_from_Europe.js
+++ 7kyu/JavaScript_developers_coming_from_Europe.js
@@ -1,18 +1,24 @@
 /* eslint-disable import/prefer-default-export */
 // https://www.codewars.com/kata/582746fa14b3892727000c4f/train/javascript
 
-// ORIGINAL SOLUTION
+// OPTIMISED SOLUTION USING FILTER
 export function countDevelopers(list) {
-  // declare counter variable
-  let euroDevCounter = 0;
-  // iterate through devs array
-  list.forEach((dev) => {
-    // if continent is Europe and language is JavaScript
-    if ((dev.continent === 'Europe') && (dev.language === 'JavaScript')) {
-      // add one to the counter
-      euroDevCounter += 1;
-    }
-  });
-  // return the value of the counter
-  return euroDevCounter;
+  const euroJavaScriptDevs = list.filter((dev) => (dev.continent === 'Europe') && (dev.language === 'JavaScript'));
+  return euroJavaScriptDevs.length;
 }
+
+// // ORIGINAL SOLUTION
+// export function countDevelopers(list) {
+//   // declare counter variable
+//   let euroDevCounter = 0;
+//   // iterate through devs array
+//   list.forEach((dev) => {
+//     // if continent is Europe and language is JavaScript
+//     if ((dev.continent === 'Europe') && (dev.language === 'JavaScript')) {
+//       // add one to the counter
+//       euroDevCounter += 1;
+//     }
+//   });
+//   // return the value of the counter
+//   return euroDevCounter;
+// }
