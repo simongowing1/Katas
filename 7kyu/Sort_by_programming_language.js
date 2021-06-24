edc56fc4fd0b1aa2725264aa71e8140a00f66539
Simon Gowing <simongowing1@gmail.com>
2021-06-24 16:38:03 +0200

    refactor(sort_by_programming_language): add comments to explain kata

--- 7kyu/Sort_by_programming_language.js
+++ 7kyu/Sort_by_programming_language.js
@@ -3,16 +3,27 @@
 // https://www.codewars.com/kata/583ea278c68d96a5fd000abd
 
 export function sortByLanguage(list) {
-  return list.sort((firstEl, secondEl) => {
-    if (firstEl.language === secondEl.language) {
-      if (firstEl.firstName > secondEl.firstName) {
+  // sort the argument array, declaring the two elements to be compared
+  return list.sort((firstDev, secondDev) => {
+    // if the languages used by the devs are the same
+    if (firstDev.language === secondDev.language) {
+      // and if the first name is 'greater'
+      // ie further away from 'a' in the alphabet
+      if (firstDev.firstName > secondDev.firstName) {
+        // return it in position 1
         return 1;
       }
+      // else return it infront of position 1
       return -1;
     }
-    if (firstEl.language > secondEl.language) {
+    // else...
+    // if the language is 'greater' than the other
+    // ie further away from 'a' in the alphabet
+    if (firstDev.language > secondDev.language) {
+      // return it in position 1
       return 1;
     }
+    // else return it infront of position 1
     return -1;
   });
 }
